370a5e9074593ba7d539abae84d2cb48b23e47e6
ath0rus <65640442+ath0rus@users.noreply.github.com>
2021-10-25 14:05:31 +1000

    udpate

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import messagebox
-# import cv2 as cv
+import qrcode as qr
+import cv2 as cv
+
+image = ""
 
 ##Check version/ test pop up
 # tkinter._test()
@@ -9,12 +12,21 @@ from tkinter import messagebox
 window = tk.Tk()
 window.title("QR Code Generator")
 # window.geometry("200x150")
-# img = cv.imread("qr.png")
+# 
 
 #button functions
 def test():
     tk.messagebox.showinfo("Test", e1.get())
     # cv.imshow("Image", img)
+
+def genqr():
+    global img
+    img = qr.make(e1.get())
+    e1.delete(0, 'end')
+    img.save("qrcode1.png")
+    tk.messagebox.showinfo("QR Code", "Your qr code was made")
+
+# def open()
 
 
 L1 = tk.Label(window, text = "Text/ Url")
@@ -23,7 +35,7 @@ L1.grid(row = 0, column= 0)
 e1 = tk.Entry(window)
 e1.grid(row = 0, column = 1)
 
-B1 = tk.Button(window, text = "Generate", command=test)
+B1 = tk.Button(window, text = "Generate", command=genqr)
 B1.grid(row = 1, column= 0, columnspan= 2)
 
 #ensures that the window stays open untill the user or code closes it
---
+++ todo.md
@@ -0,0 +1,4 @@
+### Things that need to be added before this branch is merger with main
+- Display ability
+- add ability to preview before save
+- add custom save path
